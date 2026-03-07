abe38d5267a1a37f4571d56401303e3c54cd8fc3
devesa01 <joaodevesa2019@gmail..com>
2026-03-07 23:55:19 +0000

    feat: update main app configuration with constants for title and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:drena_conversa/core/constants/app_strings.dart';
+import 'package:drena_conversa/core/theme/app_theme.dart';
 import 'package:drena_conversa/features/auth/presentation/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Drena Conversa',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: AppStrings.appName,
+      theme: AppTheme.dark,
       initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
