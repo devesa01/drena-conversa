e87d9ded1f8ae444fc625900e743d9198fc2d5a9
devesa01 <joaodevesa2019@gmail..com>
2026-03-08 00:08:45 +0000

    feat: enhance CustomLoginInput with validation and improved styling

--- lib/shared/widgets/custom_login_input.dart
+++ lib/shared/widgets/custom_login_input.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
 
 class CustomLoginInput extends StatelessWidget {
   final String label;
   final bool obscureText;
   final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
 
   const CustomLoginInput({
     super.key,
     required this.label,
     this.obscureText = false,
     this.controller,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction = TextInputAction.next,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
       obscureText: obscureText,
-      style: const TextStyle(color: Colors.white),
+      validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      style: const TextStyle(color: AppColors.textPrimary),
       decoration: InputDecoration(
         labelText: label,
-        labelStyle: const TextStyle(color: Colors.white70),
+        labelStyle: const TextStyle(color: AppColors.textSecondary),
+        errorStyle: const TextStyle(color: Colors.redAccent),
         enabledBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.white54),
+          borderSide: BorderSide(color: AppColors.inputBorder),
         ),
         focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.white),
+          borderSide: BorderSide(color: AppColors.inputBorderFocused),
+        ),
+        focusedErrorBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.redAccent),
+        ),
+        errorBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.redAccent),
         ),
       ),
     );
