914aa61f8a4cf0ee08b7361ffe26e406e3471d24
devesa01 <joaodevesa2019@gmail..com>
2026-03-08 00:08:33 +0000

    feat: implement validation methods for username and password

--- lib/core/utils/validators.dart
+++ lib/core/utils/validators.dart
@@ -1,3 +1,19 @@
+import 'package:drena_conversa/core/constants/app_strings.dart';
+
 class Validators {
-  
+  Validators._();
+
+  static String? username(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return AppStrings.errorUsernameRequired;
+    }
+    return null;
+  }
+
+  static String? password(String? value) {
+    if (value == null || value.isEmpty) {
+      return AppStrings.errorPasswordRequired;
+    }
+    return null;
+  }
 }
