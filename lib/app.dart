ffd52b5ec956e364279681737e2b345c5a5ecb1b
devesa01 <joaodevesa2019@gmail..com>
2026-03-09 22:52:34 +0000

    refactor: app structure by creating AppRoutes and MyApp classes for improved navigation

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:drena_conversa/core/constants/app_strings.dart';
+import 'package:drena_conversa/core/theme/app_theme.dart';
+import 'package:drena_conversa/features/auth/presentation/screens/login_screen.dart';
+import 'package:drena_conversa/features/chat/screen/chat_menu_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  AppRoutes._();
+
+  static const String login = '/login';
+  static const String chatMenu = '/chatmenu';
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppStrings.appName,
+      theme: AppTheme.dark,
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.login,
+      routes: {
+        AppRoutes.login: (context) => const LoginScreen(),
+        AppRoutes.chatMenu: (context) => const ChatMenuScreen(),
+      },
+    );
+  }
+}
